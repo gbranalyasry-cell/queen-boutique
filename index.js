d1219e07568dc5a5d353438024256f9241b0d41a
gbranalyasry-cell <gbranalyasry@gmail.com>
2025-09-18 00:37:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,1 @@
+import React from 'react';import ReactDOM from 'react-dom/client';import QueenBoutique from './QueenBoutique';const root = ReactDOM.createRoot(document.getElementById('root'));root.render(<QueenBoutique />);
